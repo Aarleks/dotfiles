ac3da2fdb0d76689db6b400681fca4413a3a0e43
Alex Norman <Aarleks@gmail.com>
2019-04-19 18:25:35 +1000

    Changed dotfiles repo location

--- strappyboot.sh
+++ strappyboot.sh
@@ -27,7 +27,7 @@ while getopts ":a:r:p:h" o; do case "${o}" in
 esac done
 
 # DEFAULTS:
-[ -z ${dotfilesrepo+x} ] && dotfilesrepo="https://github.com/Aarleks/configdump.git"
+[ -z ${dotfilesrepo+x} ] && dotfilesrepo="https://github.com/Aarleks/dotfiles.git"
 [ -z ${progsfile+x} ] && progsfile="https://raw.githubusercontent.com/Aarleks/dotfiles/master/programs.csv"
 [ -z ${aurhelper+x} ] && aurhelper="yay"
 
