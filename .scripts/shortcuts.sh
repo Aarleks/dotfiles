4709f7eb302e870efa78e311a10381accbd824fe
Alex Norman <Aarleks@gmail.com>
2020-04-03 18:29:17 +1100

    schortcuts

--- .scripts/shortcuts.sh
+++ .scripts/shortcuts.sh
@@ -10,9 +10,9 @@ printf "# vim:filetype=vim\\n" > "$lf_shortcuts"
 
 # Format the `bmdirs` file in the correct syntax and sent it to all three configs.
 sed "s/\s*#.*$//;/^\s*$/d" "$HOME/.config/alias-dirs" | tee >(awk '{print $1"=\"cd "$2" && ls -a\" \\"}' >> "$shell_shortcuts") \
-    >(awk '{print "map g" $1, "cd", $2 "\nmap M" $1, "$(mv $f", $2 ")\nmap C" $1, "$(cp $f", $2 ")" }' >> "$lf_shortcuts")
+    >(awk '{print "map g" $1, "cd", $2 "\nmap M" $1, "$(mv $f", $2 ")\nmap C" $1, "$(cp $f", $2 ")" }' >> "$lf_shortcuts") \
 
 # Format the `configs` file in the correct syntax and sent it to both configs.
 sed "s/\s*#.*$//;/^\s*$/d"  "$HOME/.config/alias-conf" | tee >(awk '{print $1"=\"$EDITOR "$2"\" \\"}' >> "$shell_shortcuts") \
-    >(awk '{print "map", $1, ":e", $2 "<CR>" }' >> "$lf_shortcuts")
+    >(awk '{print "map", $1, ":e", $2 "<CR>" }' >> "$lf_shortcuts") \
 
