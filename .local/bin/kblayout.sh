3ea02c016a6ba998c74bacbe922f85423f1a1fa3
Alex Norman <Aarleks@gmail.com>
2020-04-04 15:16:01 +1100

    Make kblayout posix shell compliant

---
+++ .local/bin/kblayout.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+key=$(xmodmap -pke | grep -w '33' | awk "{print \$NF}")
+# If the keyboard is set to US layout this will print "P".
+
+if [ "$key" = "P" ]; then
+	setxkbmap us -variant colemak
+	# Add another function in here.
+else
+	setxkbmap us
+	# Add another function in here.
+fi
