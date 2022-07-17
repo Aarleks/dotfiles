e01a4aa221f18745d1f8bba29f62c1593aa35adb
Alex Norman <Aarleks@gmail.com>
2022-07-17 19:42:48 +1000

    Add config files and executables

--- .local/bin/kblayout.sh
+++ .local/bin/kblayout.sh
@@ -4,9 +4,9 @@ key=$(xmodmap -pke | grep -w '33' | awk "{print \$NF}")
 # If the keyboard is set to US layout this will print "P".
 
 if [ "$key" = "P" ]; then
-	setxkbmap us -variant colemak
-	# Add another function in here.
+	setxkbmap -layout us -variant colemak -option caps:super && killall xcape 2>/dev/null ; xcape -e 'Super_L=Escape'
+
 else
-	setxkbmap us
+	setxkbmap -layout us -option caps:super && killall xcape 2>/dev/null ; xcape -e 'Super_L=Escape'
 	# Add another function in here.
 fi
